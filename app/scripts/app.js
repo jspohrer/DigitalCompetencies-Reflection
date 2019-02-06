e485b0895100fea1ccd0082064c48a8c7d62a291
Leslie Goloh <leslieg@wc137-dhcp-35.brynmawr.edu>
2019-02-06 16:27:29 -0500

    updates

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -110,7 +110,7 @@
 
   function printToPDF() {
     var summaryPDF = "YourResults.pdf";
-    var quality = 5
+    var quality = 7
     html2canvas(document.querySelector("#convertTopdf"), {scale: quality}).then(canvas => {
       let pdf = new jsPDF('p', 'mm', 'a4');
       pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
