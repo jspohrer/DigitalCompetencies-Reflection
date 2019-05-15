017c03e87ec8f3be63b216588910f38d998c41da
Leslie Goloh <leslieg@wc137-dhcp-35.brynmawr.edu>
2019-05-14 23:36:33 -0400

    heroku commit

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const express = require('express'),
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')))
 app.use(express.static(path.join(__dirname, '/views')));
 
