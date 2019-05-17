fce1796073beccd6efb9da6fd48bc3a73ea8bcc9
Leslie Goloh <leslieg@wc137-dhcp-35.brynmawr.edu>
2019-05-17 10:23:36 -0400

    heroku commit

--- server/model/sqlDB.js
+++ server/model/sqlDB.js
@@ -5,9 +5,9 @@ var sqldatabase = require('mysql');
 var pool = sqldatabase.createPool({
   connectionLimit : 20,
   host        : '67.205.172.52',
-  user        : 'lgolohdi_user',
-  password    : 'SebastianBachFugue#1',
-  database    : 'lgolohdi_TestDatabase',
+  user        : 'jspohrer_dbadmin',
+  password    : '_UgyPccD(,EQ',
+  database    : 'jspohrer_digicompapp',
   port        : '3306'
 });
 
